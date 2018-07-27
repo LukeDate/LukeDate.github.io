1503dbdebeb0a0c4f08024b68261f34c9457ad74
Luke Date <lukedate@Lukes-MacBook-Pro.local>
2018-07-27 12:46:41 +0100

    first commit to Master branch

---
+++ components/InputPreview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+export default class InputPreview extends Component {
+  render() {
+    return (
+      <div>
+        <input type='text'/>
+      </div>
+    )
+  }
+}
