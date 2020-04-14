865c3d1b5e051f60d5a90a484ea3177e4cf6b969
LukeDate <34647960+LukeDate@users.noreply.github.com>
2020-04-14 15:57:56 -0700

    changed picture sizes faster loading

--- src/pages/hamsa.js
+++ src/pages/hamsa.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 const Hamsa = () => {
+    //determine if string has all unique characters
+    let s1 = "interview"; //true
+    let s2 = "elephant"; //false
+    let s3 = "unique"; //false
+    let s4 = "brandy"; //true
+
+    //intial thought, use object.
+    const uniqueChars = (str) => {
+        let strArr = str.split('');
+        let charObj = {
+        };
+        for(var i = 0; i < strArr.length; i++) {
+            let word = strArr[i];
+            charObj[word] = charObj[word] ? 'FALSE' : 1;
+            if(charObj[word] === 'FALSE') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     return (
         <div>
-            Coming Soon
+            {
+                console.log(uniqueChars(s1)
+                )
+            
+            }
         </div>
     )
 }
--- src/pages/photos.js
+++ src/pages/photos.js
@@ -12,12 +12,12 @@ const Photos = () => {
         //how should I have gallery? Grid options with button? All photos? Section? (astro, landscape, artsy ones)
         <div>
             <h1>Astrophotography</h1>
-            <Image src="/photos/DSC_0193.jpg" />
-            <Image src="/photos/DSC_0195.jpg"/>    
-            <Image src="/photos/DSC_0202.jpg"/>    
-            <Image src="/photos/DSC_0206.jpg"/>    
-            <Image src="/photos/DSC_0200.jpg"/>    
-            <Image src="/photos/DSC_0205.jpg"/>
+            <Image width="25%" height="25%" src="/photos/DSC_0193.jpg" />
+            <Image width="25%" height="25%" src="/photos/DSC_0195.jpg"/>    
+            <Image width="25%" height="25%" src="/photos/DSC_0202.jpg"/>    
+            <Image width="25%" height="25%" src="/photos/DSC_0206.jpg"/>    
+            <Image width="25%" height="25%" src="/photos/DSC_0200.jpg"/>    
+            <Image width="25%" height="25%" src="/photos/DSC_0205.jpg"/>
             <h2 style={{fontStyle: 'italic', marginBottom: '0'}}>More coming soon...</h2> 
         </div>
     )
